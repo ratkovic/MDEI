1123f53911b85f6b39188a07e67a060e9ca35145
thinkinavi24 <70708980+thinkinavi24@users.noreply.github.com>
2021-06-10 15:16:37 -0400

    Add files via upload

--- NithinPackage/src/bayesLassoRevised.cpp
+++ NithinPackage/src/bayesLassoRevised.cpp
@@ -4,6 +4,7 @@
 #include "RcppArmadillo.h"
 
 using namespace arma;
+using namespace Rcpp;
 using namespace std;
 
 // via the depends attribute we tell Rcpp to create hooks for
@@ -27,7 +28,7 @@ using namespace std;
 //' @export
 
 //[[Rcpp::export]]
-Rcpp::List bayesLasso(vec y, mat X, vec alpha, double tol) { //tol is 1e-6
+List bayesLasso(vec y, mat X, double alpha, double tol) { //tol is 1e-6, just takes in one alpha
   
   int n = X.n_rows;
   int p = X.n_cols;
@@ -59,8 +60,15 @@ Rcpp::List bayesLasso(vec y, mat X, vec alpha, double tol) { //tol is 1e-6
       }
       beta_last = beta;
       // Only select submatrix after the first iteration
-      if(i == 0) uvec update_ind = find(abs(beta) > -1);
-      if(i > 0) uvec update_ind = find(abs(beta) > tol*sdy);
+      
+      uvec update_ind;
+      
+      if(i == 0) {
+        update_ind = find(abs(beta) > -1);  
+      }
+      else {
+        update_ind = find(abs(beta) > tol*sdy); 
+      }
       
       beta(update_ind) = solve(XpXsolve.submat(update_ind,update_ind),Xpy.rows(update_ind));
       fits = X*beta;
@@ -71,7 +79,7 @@ Rcpp::List bayesLasso(vec y, mat X, vec alpha, double tol) { //tol is 1e-6
       Etausqinv(0) = 0;
       Ewtsqtausq(0) = 0;
       
-      lambda_0 = sqrt((alpha(0) - 1)/(sum(Ewtsqtausq)/2 + 1));
+      lambda_0 = sqrt((alpha - 1)/(sum(Ewtsqtausq)/2 + 1));
       sigma_sq = (sum((y - fits) % (y - fits)) + sum(beta % beta % Etausqinv/2))/(n/2 + p/2 + 1);
       sigma = sqrt(sigma_sq);
       
@@ -83,25 +91,170 @@ Rcpp::List bayesLasso(vec y, mat X, vec alpha, double tol) { //tol is 1e-6
     GCV = sum((y-fits)%(y-fits))/(den*den);
   }
   
-  return Rcpp::List::create(
-    Rcpp::Named("intercept") = beta(0),
-    Rcpp::Named("coefficients") = beta.rows(1,p-1),
-    Rcpp::Named("fitted.values") = fits,
-    Rcpp::Named("GCV") = GCV,
+  return List::create(
+    Named("intercept") = beta(0),
+    Named("coefficients") = beta.rows(1,p-1),
+    Named("fitted.values") = fits,
+    Named("GCV") = GCV,
     Rcpp::Named("Etausqinv") = Etausqinv
   );
 }
+
+//[[Rcpp::export]]
+vec updateEtausqinv(vec y, mat X, double alpha, vec Etausqinv, double tol) { 
+  int n = X.n_rows;
+  int p = X.n_cols;
+  vec Ewtsqtausq = ones(p);
+  mat XpX = X.t()*X;
+  mat Xpy = X.t()*y;
+  mat XpXsolve = XpX;
+  vec fits = zeros(n);
+  double lambda_0 = 1;
+  
+  double sdy = stddev(y);
+  double sigma_sq = sdy*sdy;
+  double sigma = sdy;
+  double conv = 1;
+  double edf = 0;
+  double GCV = 0;
+  
+  vec beta = zeros(p);
+  vec beta_last = beta;
+  
+  int iters = 500; //I like to put constants like this as their own variables, so we can change the value later easily
+  
+  for (int i = 0; i < iters; ++i) {
+    
+    if (conv/sdy > tol) {
+      for (int j = 1; j < p; ++j) {
+        XpXsolve(j, j) = XpX(j, j) + Etausqinv(j) + 1e-6;    
+      }
+      beta_last = beta;
+      // Only select submatrix after the first iteration
+      
+      uvec update_ind;
+      
+      if(i == 0) {
+        update_ind = find(abs(beta) > -1);  
+      }
+      else {
+        update_ind = find(abs(beta) > tol*sdy); 
+      }
+      
+      beta(update_ind) = solve(XpXsolve.submat(update_ind,update_ind),Xpy.rows(update_ind));
+      fits = X*beta;
+      for (int j= 1; j < p; ++j) {
+        Ewtsqtausq(j)  = abs(beta(j))/(lambda_0*sigma)+pow(lambda_0,-2);
+        Etausqinv(j)  = lambda_0/abs(beta(j))*sigma;
+      }
+      Etausqinv(0) = 0;
+      Ewtsqtausq(0) = 0;
+      
+      lambda_0 = sqrt((alpha - 1)/(sum(Ewtsqtausq)/2 + 1));
+      sigma_sq = (sum((y - fits) % (y - fits)) + sum(beta % beta % Etausqinv/2))/(n/2 + p/2 + 1);
+      sigma = sqrt(sigma_sq);
+      
+      conv = max(abs(beta - beta_last));
+    }
+    uvec update_ind = find(abs(beta) > tol*sdy );
+    edf = trace(XpX.submat(update_ind,update_ind)*pinv(XpXsolve.submat(update_ind,update_ind)));
+    double den = (n-log(n)/2*edf);
+    GCV = sum((y-fits)%(y-fits))/(den*den);
+  }
+  return Etausqinv; //List::create(GCV, Etausqinv);
+}
+
+//[[Rcpp::export]]
+double G(vec y, mat X, double alpha, vec Etausqinv, double tol) { 
+  int n = X.n_rows;
+  int p = X.n_cols;
+  vec Ewtsqtausq = ones(p);
+  mat XpX = X.t()*X;
+  mat Xpy = X.t()*y;
+  mat XpXsolve = XpX;
+  vec fits = zeros(n);
+  double lambda_0 = 1;
+  
+  double sdy = stddev(y);
+  double sigma_sq = sdy*sdy;
+  double sigma = sdy;
+  double conv = 1;
+  double edf = 0;
+  double GCV = 0;
+  
+  vec beta = zeros(p);
+  vec beta_last = beta;
+  
+  int iters = 500; //I like to put constants like this as their own variables, so we can change the value later easily
+  
+  for (int i = 0; i < iters; ++i) {
+    
+    if (conv/sdy > tol) {
+      for (int j = 1; j < p; ++j) {
+        XpXsolve(j, j) = XpX(j, j) + Etausqinv(j) + 1e-6;    
+      }
+      beta_last = beta;
+      // Only select submatrix after the first iteration
+      
+      uvec update_ind;
+      
+      if(i == 0) {
+        update_ind = find(abs(beta) > -1);  
+      }
+      else {
+        update_ind = find(abs(beta) > tol*sdy); 
+      }
+      
+      beta(update_ind) = solve(XpXsolve.submat(update_ind,update_ind),Xpy.rows(update_ind));
+      fits = X*beta;
+      for (int j= 1; j < p; ++j) {
+        Ewtsqtausq(j)  = abs(beta(j))/(lambda_0*sigma)+pow(lambda_0,-2);
+        Etausqinv(j)  = lambda_0/abs(beta(j))*sigma;
+      }
+      Etausqinv(0) = 0;
+      Ewtsqtausq(0) = 0;
+      
+      lambda_0 = sqrt((alpha - 1)/(sum(Ewtsqtausq)/2 + 1));
+      sigma_sq = (sum((y - fits) % (y - fits)) + sum(beta % beta % Etausqinv/2))/(n/2 + p/2 + 1);
+      sigma = sqrt(sigma_sq);
+      
+      conv = max(abs(beta - beta_last));
+    }
+    uvec update_ind = find(abs(beta) > tol*sdy );
+    edf = trace(XpX.submat(update_ind,update_ind)*pinv(XpXsolve.submat(update_ind,update_ind)));
+    double den = (n-log(n)/2*edf);
+    GCV = sum((y-fits)%(y-fits))/(den*den);
+  }
+  return GCV; //List::create(GCV, Etausqinv);
+}
+
+//[[Rcpp::export]]
+vec GCV(vec y, mat X, vec alphas, vec Etausqinv, double tol) {
+  vec GCVs = alphas;
+  int p = alphas.n_rows;
+  for (int i = 0; i < p; ++i) {
+    Etausqinv = updateEtausqinv(y, X, alphas(i), Etausqinv, tol);
+    //Etausqinv = tuple[1];
+    GCVs(i) = G(y, X, alphas(i), Etausqinv, tol);
+  }
+    
+  return GCVs;
+}
+
 //[[Rcpp::export]]
 int main() {
-    vec y{3, 1, 4, 1};
-    mat X{{2, 7, 1},{8, 2, 8}, {1, 8, 2}, {8, 4, 5}};
-    
-    vec alpha{2};
-    double tol = 1e-6;
-    
-    print(bayesLasso(y, X, alpha, tol));
-    
-    return 0;
+  vec y{3, 1, 4, 1};
+  mat X{{2, 7, 1},{8, 2, 8}, {1, 8, 2}, {8, 4, 5}};
+  
+  vec alphas{1.2,3.4,5.6,7.8};
+  vec Etausqinv{1, 1, 1};
+  double tol = 1e-6;
+  //alphas.print();
+  //cout << alphas.n_rows << endl;
+  vec GCVs = GCV(y, X, alphas, Etausqinv, tol);
+  GCVs.print();
+  
+  return 0;
 }
 
 /*** R
