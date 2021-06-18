67a88a3dd4ae727305686e00521c529bc86d0e12
thinkinavi24 <70708980+thinkinavi24@users.noreply.github.com>
2021-06-18 17:51:59 -0400

    Delete NithinPackage directory

--- NithinPackage/development/helper_NithinPackage.R
+++
@@ -1,5 +0,0 @@
-Rcpp::compileAttributes('~/Dropbox/Github/NithinPackage')           # this updates the Rcpp layer from C++ to R
-
-## Not used yet
-#roxygen2::roxygenize('~/Dropbox/Github/NithinPackage', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
-#roxygen2::roxygenize('~/Dropbox/Github/NithinPackage')  # this updates the documentation based on roxygen comments
