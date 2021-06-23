8cf1dd1f33b67b6e1c9ce29cb11a86651860f15e
ratkovic <ratkovic@users.noreply.github.com>
2021-06-23 17:06:32 -0400

    Updating helper file

---
+++ NithinPackage/development/helper.R
@@ -0,0 +1,4 @@
+"C:\Users\think\OneDrive\Documents\NithinPackage"
+
+Rcpp::compileAttributes('~/Dropbox/Github/Warmup/NithinPackage')           # this updates the Rcpp layer from C++ to R
+roxygen2::roxygenize('~/Dropbox/Github/Warmup/NithinPackage', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
