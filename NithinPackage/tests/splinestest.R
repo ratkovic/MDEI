af65a93d1d0b33242113a497b0b6160147cb950c
ratkovic <ratkovic@users.noreply.github.com>
2021-07-06 10:09:19 -0400

    Update splinestest.R

--- NithinPackage/tests/splinestest.R
+++ NithinPackage/tests/splinestest.R
@@ -1,9 +1,16 @@
 devtools::load_all('~/Dropbox/Github/warmup/NithinPackage')
 
+library(testthat)
+
 n <- 500
 p <- 5
 
 X <- matrix(rnorm(n*p), nrow = n)
+X2 <- NithinPackage:::bsme(X)
 
+dim(X)
+dim(X2)
 
-X2<-NithinPackage:::bsme(X)
+y<- rnorm(n)
+
+X2 <- NithinPackage::splineBasesAndCorrs(n, p, X, letters[1:p], y, y, "treat", 100)
