79fdbe491d1471ff62396c8426e85bbbc7e02bd9
ratkovic <ratkovic@users.noreply.github.com>
2021-08-11 22:57:39 -0400

    Loads now
    Added usedynlib to roxygen.r, added a blank line to description

---
+++ R/roxygen.R
@@ -0,0 +1,12 @@
+#' MDEI
+#'
+#' @description MDEI estimates  effects.
+#' @aliases NULL MDEI-package
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib MDEI
+#' @importFrom Rcpp sourceCpp
+#' @import stats
+## usethis namespace: end
+NULL
--- development/helper.R
+++ development/helper.R
@@ -5,3 +5,4 @@ roxygen2::roxygenize('C:\\Users\\think\\OneDrive\\Documents\\NithinPackage', roc
 
 Rcpp::compileAttributes('~/Dropbox/Github/MDEI')           # this updates the Rcpp layer from C++ to R
 roxygen2::roxygenize('~/Dropbox/Github/MDEI', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
+roxygen2::roxygenize('~/Dropbox/Github/MDEI')  # this updates the documentation based on roxygen comments
