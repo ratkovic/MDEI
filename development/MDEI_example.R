083688c2852d69d7638a255bd3d613eff35c893e
ratkovic <ratkovic@users.noreply.github.com>
2021-08-13 09:45:05 -0400

    Create MDEI_example.R

---
+++ development/MDEI_example.R
@@ -0,0 +1,31 @@
+library(MDEI)
+library(tictoc)
+n <- 10000
+
+X <- matrix(rnorm(n*5), nrow = n)
+treat <- rnorm(n)+X[,2]
+
+theta.true <- treat^2
+theta.true <- theta.true - mean(theta.true)
+tau.true <- 2*treat
+
+y <- theta.true + X[,2] + rnorm(n)
+
+set.seed(1)
+tic()
+m1 <- MDEI(y, treat, X, splits=10, alpha=.9)
+toc()
+
+cor(m1$tau.est, tau.true)
+cor(m1$theta.est, theta.true)
+
+plot(treat,tau.true,type="l")
+points(treat,m1$tau.est, pch=19, cex=.5)
+
+
+plot(sort(treat),sort(treat)^2-mean(sort(treat)^2), type="l", ylim=range(m1$theta.est))
+points(treat,m1$theta.est-mean(m1$theta.est), pch=19, cex=.5)
+
+
+mean(apply(m1$CIs.tau-2*treat,1,prod)<0)
+
