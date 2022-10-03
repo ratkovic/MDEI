390d4ce85c02fa6a985fb6ff404b1cfc37b19405
ratkovic <ratkovic@users.noreply.github.com>
2022-10-03 11:13:34 -0400

    Recompiling manual

--- development/helper_MDEI.R
+++ development/helper_MDEI.R
@@ -1,8 +1,8 @@
 Rcpp::compileAttributes('~/Dropbox/Github/MDEI')           # this updates the Rcpp layer from C++ to R
 roxygen2::roxygenize('~/Dropbox/Github/MDEI', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
 roxygen2::roxygenize('~/Dropbox/Github/MDEI')  # this updates the documentation based on roxygen comments
-
-Rcpp::compileAttributes('~/Downloads/MDEI')           # this updates the Rcpp layer from C++ to R
-roxygen2::roxygenize('~/Downloads/MDEI', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
-roxygen2::roxygenize('~/Downloads/MDEI')  # this updates the documentation based on roxygen comments
+# 
+# Rcpp::compileAttributes('~/Downloads/MDEI')           # this updates the Rcpp layer from C++ to R
+# roxygen2::roxygenize('~/Downloads/MDEI', roclets="namespace", clean=TRUE)  # this updates the documentation based on roxygen comments
+# roxygen2::roxygenize('~/Downloads/MDEI')  # this updates the documentation based on roxygen comments
 
